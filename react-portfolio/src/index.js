fbe303df69696a5ac0dacc0231524cffb3efb7ad
Nicholas Meade <nicholasjjmeade@gmail.com>
2022-08-26 15:58:39 -0400

    changes for hashrouter

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import './App.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
+    <HashRouter
+    basename='/reactportfolio'>
       <App />
-    </Router>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
