11b4d9f2ae4d0a586480ef7ab359275ee4f2a33c
Nicholas Meade <nicholasjjmeade@gmail.com>
2022-08-26 16:40:20 -0400

    navbar links fixed

--- react-portfolio/src/Components/Navbar/Navbar.js
+++ react-portfolio/src/Components/Navbar/Navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
     return ( 
       <div className="navbar" id="bootstrap-overrides">
         <header className="navbar-header" id="bootstrap-overrides">
-          <button className="logo-button"><Link to="/" style={{textDecoration: "none", color: "black"}}>Nicholas Meade</Link></button>
+          <button className="logo-button"><Link to="/reactportfolio/" style={{textDecoration: "none", color: "black"}}>Nicholas Meade</Link></button>
           {/* <button className="about-button">About Me</button>
           <button className="projects-button">Projects</button>
           <button className="skills-button">Skills</button> */}
-          <button className="connect-button"><Link to="/connect" style={{textDecoration: "none", color: "black"}}>Connect</Link></button>
+          <button className="connect-button"><Link to="/reactportfolio/connect" style={{textDecoration: "none", color: "black"}}>Connect</Link></button>
         </header>
       </div>
      );
